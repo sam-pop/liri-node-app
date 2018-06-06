09fe8437204e5d42d006e4e7681d19f5d73a22e7
sam-pop <poplovitch.samuel@gmail.com>
2018-06-05 20:56:09 -0400

    liri js update

---
+++ liri.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
