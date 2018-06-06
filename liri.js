2e300765c2cfe505ba9d86c15a0f3e98d5d952ab
sam-pop <poplovitch.samuel@gmail.com>
2018-06-05 21:09:07 -0400

    update

--- liri.js
+++ liri.js
@@ -6,3 +6,25 @@ var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+var arg1 = process.argv[2];
+var arg2 = process.argv[3];
+var argsArray = process.argv;
+
+
+switch (arg1) {
+    case 'my-tweets':
+        showMyTweets(); //TODO: build this function
+        break;
+    case 'spotify-this-song':
+        spotifySong(arg2); //TODO: build this function FIXME: make sure this is the arg the function need
+        break;
+    case 'movie-this':
+        omdbMovie(arg2); //TODO: build this function FIXME: make sure this is the arg the function need
+        break;
+    case 'do-what-it-says':
+        runRandomTxt(); //TODO: build this function
+        break;
+    default:
+        break;
+}
