8fba0223320fec2965d1edd9c39a5bbf25fedc7b
sam-pop <poplovitch.samuel@gmail.com>
2018-06-05 22:23:17 -0400

    argsArray update

--- liri.js
+++ liri.js
@@ -10,22 +10,20 @@ const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
 
 // Variables
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
 var argsArray = process.argv;
 // removes the first two elements (paths) from the args array
 argsArray.splice(0, 2);
 
 // run the appropriate function depends on the user input
-switch (arg1) {
+switch (argsArray[0]) {
     case 'my-tweets':
         showMyTweets(); //TODO: build this function
         break;
     case 'spotify-this-song':
-        spotifySong(arg2); //TODO: build this function FIXME: make sure this is the arg the function need
+        spotifySong(argsArray[1]); //TODO: build this function FIXME: make sure this is the arg the function need
         break;
     case 'movie-this':
-        omdbMovie(arg2); //TODO: build this function FIXME: make sure this is the arg the function need
+        omdbMovie(argsArray[1]); //TODO: build this function FIXME: make sure this is the arg the function need
         break;
     case 'do-what-it-says':
         runRandomTxt(); //TODO: build this function
