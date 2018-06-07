341143252a1e6ce932b7f545eb581bc956703e2e
sam-pop <poplovitch.samuel@gmail.com>
2018-06-06 23:15:32 -0400

    refactoring

--- liri.js
+++ liri.js
@@ -13,16 +13,16 @@ const OMDB_KEY = "trilogy";
 
 // Variables
 const argsArray = process.argv.slice(2); // without the first two elements (paths)
-const OMDB_URL = "http://www.omdbapi.com/?apikey=" + OMDB_KEY + "&plot=short&t="; //FIXME: add the query URL
+const OMDB_URL = "http://www.omdbapi.com/?apikey=" + OMDB_KEY + "&plot=short&t=";
 
 
 // run the appropriate function depends on the user input
 switch (argsArray[0]) {
     case 'my-tweets':
-        showMyTweets(); //TODO: build this function
+        showMyTweets();
         break;
     case 'spotify-this-song':
-        spotifySong(argsArray[1]); //TODO: build this function FIXME: make sure this is the arg the function need
+        spotifySong(argsArray[1]); //TODO: finish building this function
         break;
     case 'movie-this':
         omdbMovie(argsArray[1]);
@@ -30,9 +30,8 @@ switch (argsArray[0]) {
     case 'do-what-it-says':
         runRandomTxt(); //TODO: build this function
         break;
-    default:
-        break;
 }
+
 
 // use the twitter API package to show my last 20 tweets and when they were created
 function showMyTweets() {
@@ -46,7 +45,7 @@ function showMyTweets() {
             if (error) throw error;
             if (tweets) {
                 for (let i of tweets) {
-                    console.log("Tweet: " + i.text);
+                    console.log("Tweet: " + i.text + " (" + i.created_at + ")");
                 }
             }
         }
@@ -54,7 +53,19 @@ function showMyTweets() {
 }
 
 // This will show the following information about the song: artist(s), song's name, preview link, album (default: "The Sign" by Ace of Base)
-function spotifySong(song) {}
+//TODO: fix to display all the info
+function spotifySong(song) {
+    spotify.search({
+        type: 'track',
+        query: 'All the Small Things',
+        limit: 2
+    }, function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(data.tracks.items);
+    });
+}
 
 // show the movie info fetched from the OMDB API
 function omdbMovie(movie) {
@@ -62,22 +73,33 @@ function omdbMovie(movie) {
         movie = 'Mr. Nobody';
     }
     let url = OMDB_URL + movie;
-    console.log(url);
     request(url, (error, response, body) => {
         if (error) throw error;
         if (!error && response.statusCode === 200) {
             let thisMovie = JSON.parse(body);
-            console.log("* " + thisMovie.Title);
-            console.log("* " + thisMovie.Year);
-            console.log("* " + thisMovie.imdbRating);
-            console.log("* " + thisMovie.Ratings[1].Value);
-            console.log("* " + thisMovie.Country);
-            console.log("* " + thisMovie.Language);
-            console.log("* " + thisMovie.Plot);
-            console.log("* " + thisMovie.Actors);
+            print('Title', thisMovie.Title);
+            print('Year', thisMovie.Year);
+            print('IMDB Rating', thisMovie.imdbRating);
+            print('RT Rating', thisMovie.Ratings[1].Value);
+            moreThanOne(thisMovie.Country) ? print('Countries', thisMovie.Country) : print('Country', thisMovie.Country);
+            moreThanOne(thisMovie.Language) ? print('Languages', thisMovie.Language) : print('Language', thisMovie.Language);
+            print('Plot', thisMovie.Plot);
+            print('Actors', thisMovie.Actors);
         }
     });
 }
 
 // run the command inside the random.txt file
 function runRandomTxt() {}
+
+// print the item to the console/terminal 
+function print(title, item) {
+    console.log("* " + title + ": " + item);
+}
+
+// checks if the string holds more than one entry (separated by comma)
+function moreThanOne(str) {
+    if (str.indexOf(',') == -1)
+        return false;
+    return true;
+}
